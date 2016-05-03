1859863e5361cafb3a8ff7f8cbd7a3574d4ff226
Ryan Nagle <rmnagle@gmail.com>
2016-05-03 16:46:29 -0500

    make sure the js router works properly when urls end with 'index.html'

--- assets/js/search.js
+++ assets/js/search.js
@@ -6,13 +6,17 @@
     routes: {
       'search': 'cases',
       'search/': 'cases',
+      'search/index.html': 'cases',
       '': 'cases',
 
       'search/officers': 'officers',
       'search/cases': 'cases',
 
       'search/officers/': 'officers',
+      'search/officers/index.html': 'officers',
+
       'search/cases/': 'cases',
+      'search/cases/index.html': 'cases',
 
       'case/*path': 'case_detail',
       'officer/*path': 'officer_detail'
