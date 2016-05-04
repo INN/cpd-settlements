e1c81bd42f53bd956a461e58b497dd491f4e9d0a
Ryan Nagle <rmnagle@gmail.com>
2016-05-04 11:18:36 -0500

    change prep_officer_data to prevent duplicate records, fix relationship between officers and cases, misc related changes

--- lib/prep_officer_data.py
+++ lib/prep_officer_data.py
@@ -7,46 +7,91 @@ def prep_officer_data():
     with open('data/officers_full.json', 'r') as f:
         officers = json.loads(f.read())
 
+    result = []
+    seen = []
+
     for cop in casecops:
-        officer_data = filter(lambda x: x.get('id', None) == cop.get('cop', None), officers)[0]
+        try:
+            if (seen.index(cop.get('cop')) >= 0):
+                print "Already saw officer %s" % cop.get('cop')
+                existing_cop = result[seen.index(cop.get('cop'))]
+                print "Check ID: %s" % existing_cop.get('id')
 
-        """
-        Unset some keys we're going to replace with data from the officers sheet
-        """
-        delete_these = [
-            'cop_first_name',
-            'cop_middle_initial',
-            'cop_last_name'
-        ]
+                print "Adding case no. %s to existing data" % cop.get('case_no')
+                existing_cop['case_numbers'].append(cop.get('case_no'))
+                print ""
 
-        for key in delete_these:
-            del cop[key]
+                continue
+        except ValueError:
 
-        """
-        Use identifying info from officers sheet:
-        - First name
-        - Middle initial
-        - Last name
-        - Gender
-        - Race
-        - Dates of service
-        - Rank
-        - Unit
-        """
-        use_these = [
-            'first_name',
-            'last_name',
-            'middle_init',
-            'sex',
-            'race',
-            'appointed_date',
-            'resignation_date',
-            'position_desc',
-            'notes'
-        ]
+            officer_data = filter(lambda x: x.get('id', None) == cop.get('cop', None), officers)[0]
 
-        for key in use_these:
-            cop[key] = officer_data[key]
+            """
+            Unset some keys we're going to replace with data from the officers sheet
+            """
+            delete_these = [
+                'cop_first_name',
+                'cop_middle_initial',
+                'cop_last_name'
+            ]
+
+            for key in delete_these:
+                del cop[key]
+
+            """
+            Use identifying info from officers sheet:
+            - First name
+            - Middle initial
+            - Last name
+            - Gender
+            - Race
+            - Dates of service
+            - Rank
+            - Unit
+            """
+            use_these = [
+                'first_name',
+                'last_name',
+                'middle_init',
+                'sex',
+                'race',
+                'appointed_date',
+                'resignation_date',
+                'position_desc',
+                'notes'
+            ]
+
+            for key in use_these:
+                cop[key] = officer_data[key]
+
+            # Start a list of case numbers in the event
+            # this officer has been involved in multiple
+            cop['case_numbers'] = [cop.get('case_no'), ]
+
+            # Map fields to what we're using in the app already
+            field_map = {
+                'timestamp': "matched_when",
+                'appointed': 'appointed_date',
+                'resigned': 'resignation_date',
+                'attorney': "officer_atty",
+                'attorney_firm': "officer_atty_firm",
+                'prefix': "position_desc",
+                'badge_number': 'badge_no',
+                'case_number': 'case_no',
+                'id': 'cop',
+                'first': 'first_name',
+                'middle': 'middle_init',
+                'last': 'last_name',
+                'race': 'race',
+                'notes': 'notes'
+            }
+
+            for use, dont in field_map.items():
+                cop[use] = cop[dont]
+                del cop[dont]
+
+            result.append(cop)
+            seen.append(cop.get('id'))
 
     with open('data/officers.json', 'w+') as f:
-        f.write(json.dumps(casecops))
+        f.write(json.dumps(result))
