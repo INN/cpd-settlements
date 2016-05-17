25a7709c597444d610f8b5d7d5d5776fbba9b0fb
Julia Smith <julia@inn.org>
2016-05-17 08:08:06 -0500

    social sharing for officers and cases

--- assets/js/search.js
+++ assets/js/search.js
@@ -683,3 +683,56 @@
   });
 
 })();
+
+
+
+function case_twitter(element){
+  var $case = $(element).closest('.case, .case-detail');
+  var dollars = $case.find('h2').text();
+  var link; 
+  if ($case.find('a.case-link').get(0)) {
+    link = $case.find('a.case-link').get(0).href;  
+  } else {
+    link = window.location.href;
+  }  
+  var text = "This police settlement cost Chicago " + dollars + ". " + link + " via @chicagoreporter";
+  var twitter_url = "https://twitter.com/home?status=" + encodeURIComponent(text);
+  window.open(twitter_url, 'newwindow', 'width=600, height=400');
+}
+function case_facebook(element){
+  var $case = $(element).closest('.case, .case-detail');
+  var dollars = $case.find('h2').text();
+  var link;
+  if ($case.find('a.case-link').get(0)) {
+    link = $case.find('a.case-link').get(0).href;
+  } else {
+    link = window.location.href;
+  }
+  var facebook_url = "https://www.facebook.com/sharer/sharer.php?u=" + link; 
+  window.open(facebook_url, 'newwindow', 'width=600, height=400');
+}
+function officer_twitter(element){
+  var $officer = $(element).closest('.officer');
+  var name = $officer.find('h2').text();
+  var link;
+  if ($officer.find('a.officer-link').get(0)){
+    link = $officer.find('a.officer-link').get(0).href;
+  } else {
+    link = window.location.href;
+  }
+  var dollars = $officer.find('.total-payments').text();
+  var text = name + " was named in police settlements that cost Chicago " + dollars + ". " + link + " via @chicagoreporter";
+  var twitter_url = "https://twitter.com/home?status=" + encodeURIComponent(text);
+  window.open(twitter_url, 'newwindow', 'width=600, height=400');
+}
+function officer_facebook(element){
+  var $officer = $(element).closest('.officer');
+  var link;
+  if ($officer.find('a.officer-link').get(0)){
+    link = $officer.find('a.officer-link').get(0).href;
+  } else {
+    link = window.location.href;
+  }
+  var facebook_url = "https://www.facebook.com/sharer/sharer.php?u=" + link; 
+  window.open(facebook_url, 'newwindow', 'width=600, height=400');
+}
