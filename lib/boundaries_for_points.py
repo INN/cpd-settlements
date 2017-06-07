028b769cc00404565bcfb205833f2cfbc9c16643
Julia Smith <julia@inn.org>
2017-06-06 22:54:29 -0500

    switch from local boundary service to the Tribunes, and add a check for json data to catch errors during the boundary lookup

--- lib/boundaries_for_points.py
+++ lib/boundaries_for_points.py
@@ -4,9 +4,10 @@ import json
 import time
 
 """
-HOST_PORT = 'boundaries.tribapps.com'
-"""
 HOST_PORT = 'localhost:8000'
+"""
+
+HOST_PORT = 'boundaries.tribapps.com'
 
 
 def main():
@@ -40,34 +41,44 @@ def boundary_lookup(lat, lng):
     url = 'http://%s/1.0/boundary/?contains=%s,%s' % (HOST_PORT, lat, lng) + \
         '&sets=community-areas,neighborhoods,police-beats,police-districts'
     result = requests.get(url)
-    data = json.loads(result.content)
-    ret = {}
-    if result.status_code == 200:
-        for bound in data.get('objects'):
-            if bound.get('kind') == 'Community Area':
-                ret.update({
-                    u'community_area': bound.get('name'),
-                    u'community_area_id': bound.get('external_id'),
-                })
-            if bound.get('kind') == 'Neighborhood':
-                ret.update({
-                    u'neighborhood': bound.get('name'),
-                    u'neighborhood_id': bound.get('external_id'),
-                })
-            if bound.get('kind') == 'Police Beat':
-                ret.update({
-                    'police_beat': bound.get('name'),
-                    'police_beat_id': bound.get('external_id')
-                })
-            if bound.get('kind') == 'Police District':
-                ret.update({
-                    'police_district': bound.get('name'),
-                    'police_district_id': bound.get('external_id')
-                })
+    if is_json(result.content):
+        data = json.loads(result.content)
+        ret = {}
+        if result.status_code == 200:
+            for bound in data.get('objects'):
+                if bound.get('kind') == 'Community Area':
+                    ret.update({
+                        u'community_area': bound.get('name'),
+                        u'community_area_id': bound.get('external_id'),
+                    })
+                if bound.get('kind') == 'Neighborhood':
+                    ret.update({
+                        u'neighborhood': bound.get('name'),
+                        u'neighborhood_id': bound.get('external_id'),
+                    })
+                if bound.get('kind') == 'Police Beat':
+                    ret.update({
+                        'police_beat': bound.get('name'),
+                        'police_beat_id': bound.get('external_id')
+                    })
+                if bound.get('kind') == 'Police District':
+                    ret.update({
+                        'police_district': bound.get('name'),
+                        'police_district_id': bound.get('external_id')
+                    })
+        else:
+            print "Couldn't retrieve boundaries for: %s,%s" % (lat, lng)
     else:
         print "Couldn't retrieve boundaries for: %s,%s" % (lat, lng)
 
     return ret
+
+def is_json(myjson):
+    try:
+        json_object = json.loads(myjson)
+    except ValueError, e:
+        return False
+    return True
 
 
 if __name__ == '__main__':
