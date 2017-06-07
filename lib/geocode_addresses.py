9bee3acecc095e35aee997c1d0783cd710a38bac
Julia Smith <julia@inn.org>
2017-06-06 23:00:00 -0500

    increasing timeout length, adding comments to address points of failure, making print statements useful (prints actual data instead of statuses)

--- lib/geocode_addresses.py
+++ lib/geocode_addresses.py
@@ -15,19 +15,21 @@ def geocode_addresses():
     cases = ModelList('data/cases.json', Case, Case.field_map)
     cases_dicts = [case for case in cases]
 
-    geocoder = GoogleV3(timeout=30)
-
-    for case in cases_dicts:
+    geocoder = GoogleV3(timeout=800) # if the geocoder hits the API limit, add an api_key='YOUR-KEY-HERE' attribute with a new key from https://developers.google.com/maps/documentation/geocoding/get-api-key
+    
+    for case in cases_dicts: # if the connection times out running the whole dataset, limit the rows (example: cases_dicts[863:950] ) and collect the json manually via the print statement below
         if case.get('address', None):
-            print 'Trying to geocode address:', case.get('address')
+            #print 'Trying to geocode address:', case.get('address')
             result = geocoder.geocode('%s Chicago, IL' % case.get('address'))
             if result:
                 case['latitude'] = result.latitude
                 case['longitude'] = result.longitude
-            else:
-                print 'Could not geocode address:', case.get('address')
-        else:
-            print "No address for case: %s" % case.get('case_number')
+            #else:
+                #print 'Could not geocode address:', case.get('address')
+        #else:
+            #print "No address for case: %s" % case.get('case_number')
+
+        print json.dumps(case) # print something useful so if the connection times out you still have the data know what case it choked on
 
         time.sleep(1)
 
